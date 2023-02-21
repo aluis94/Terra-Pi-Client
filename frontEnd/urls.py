5e9e7772b26dcbd0ae7120f0f0c41e7ed4336641
Albert Luis <albertluis94@gmail.com>
2023-02-20 18:32:02 -0800

    Assignment 3

--- frontEnd/urls.py
+++ frontEnd/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('', views.home_page),
+    path('home/', views.home_page),
+    path('sensors/', views.sensors_page),
+    path('sensor/', views.sensor_page),
+    path('devices/', views.devices_page),
+    path('about/', views.about_page),
 ]
--- frontEnd/views.py
+++ frontEnd/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
+def home_page(request):
     #do stuff
     #send emails
-    return HttpResponse('Hello World')
+    return render(request, 'home.html',{'name':'albert'})
+
+def sensors_page(request):
+    #do stuff
+    #send emails
+    return render(request, 'sensors.html',{'name':'albert'})
+
+def sensor_page(request):
+    #do stuff
+    #send emails
+    return render(request, 'sensor.html',{'name':'albert'})
+
+def devices_page(request):
+    #do stuff
+    #send emails
+    return render(request, 'devices.html',{'name':'albert'})
+
+def about_page(request):
+    #do stuff
+    #send emails
+    return render(request, 'about.html',{'name':'albert'})
