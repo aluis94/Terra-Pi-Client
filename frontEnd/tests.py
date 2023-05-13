746f216dbaef2042f9ca2d4f972355fd2ee7421b
Albert Luis <albertluis94@gmail.com>
2023-05-12 17:15:24 -0700

    updates to front-end

--- frontEnd/tests.py
+++ frontEnd/tests.py
@@ -1,3 +1,291 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
-# Create your tests here.
+class TerraPiFormTest(LiveServerTestCase):
+    #This test will not create a sensor due to invalid form input
+    def testinvalidsensorform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to sensors page
+        selenium.get('http://localhost:9000/terra-pi/sensors')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'add')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        submit_button =selenium.find_element(By.ID,'submit')
+        time.sleep(1)
+        #submit form
+        submit_button.click()
+        time.sleep(2)
+
+
+    #This test will create a new sensor
+    def testsensorform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to sensors page
+        selenium.get('http://localhost:9000/terra-pi/sensors')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'add')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        sensor_name = selenium.find_element(By.ID,'name')
+        sensor_type = selenium.find_element(By.ID,'type')
+        sensor_gpio1 = selenium.find_element(By.ID,'GPIO1')
+        sensor_gpio2 = selenium.find_element(By.ID,'GPIO2')
+        sensor_gpio3 = selenium.find_element(By.ID,'GPIO3')
+        submit_button =selenium.find_element(By.ID,'submit')
+
+    
+
+        #populate the form with data
+        sensor_name.send_keys('DHT11')
+
+        #Select dropdown values
+        drop_type= Select(sensor_type)
+        drop_type.select_by_index(0)
+
+        drop_gpio1= Select(sensor_gpio1)
+        drop_gpio1.select_by_index(21)
+
+        drop_gpio2= Select(sensor_gpio2)
+        drop_gpio2.select_by_index(1)
+
+        drop_gpio3= Select(sensor_gpio3)
+        drop_gpio3.select_by_index(1)
+
+    
+
+    
+        time.sleep(1)
+        #submit form
+        #submit_button.click()
+        time.sleep(2)
+
+    #This test will not create a device due to invalid form input
+    def testinvaliddeviceform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to sensors page
+        selenium.get('http://localhost:9000/terra-pi/devices')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'add')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        submit_button =selenium.find_element(By.ID,'submit')
+        time.sleep(1)
+        #submit form
+        submit_button.click()
+        time.sleep(2) 
+
+    #This test will create a new device
+    def testdeviceform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to devices page
+        selenium.get('http://localhost:9000/terra-pi/devices')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'add')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        device_name = selenium.find_element(By.ID,'name')
+        device_type = selenium.find_element(By.ID,'type')
+        device_gpio1 = selenium.find_element(By.ID,'GPIO1')
+        device_gpio2 = selenium.find_element(By.ID,'GPIO2')
+        device_gpio3 = selenium.find_element(By.ID,'GPIO3')
+        submit_button =selenium.find_element(By.ID,'submit')
+
+    
+
+        #populate the form with data
+        device_name.send_keys('Heater1')
+        device_type.send_keys('Heater')
+        
+        #Select dropdown values
+        
+
+        drop_gpio1= Select(device_gpio1)
+        drop_gpio1.select_by_index(21)
+
+        drop_gpio2= Select(device_gpio2)
+        drop_gpio2.select_by_index(1)
+
+        drop_gpio3= Select(device_gpio3)
+        drop_gpio3.select_by_index(1)
+
+    
+
+    
+        time.sleep(1)
+        #submit form
+        #submit_button.click()
+        time.sleep(2)
+
+    #This test will not create a simple job due to invlid form inputs  
+    def testinvalidsimplejobform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to sensors page
+        selenium.get('http://localhost:9000/terra-pi/jobs')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'addsimple')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        submit_button =selenium.find_element(By.ID,'submit')
+        time.sleep(1)
+        #submit form
+        submit_button.click()
+        time.sleep(2) 
+
+    #This test will not create a multi-step (conditional) job due to invlid form inputs  
+    def testinvalidmultijobform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to sensors page
+        selenium.get('http://localhost:9000/terra-pi/jobs')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'addmulti')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        submit_button =selenium.find_element(By.ID,'submit')
+        time.sleep(1)
+        #submit form
+        submit_button.click()
+        time.sleep(2) 
+
+    def testsimplejobform(self):
+        selenium = webdriver.Chrome()
+        #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to jobs page
+        selenium.get('http://localhost:9000/terra-pi/jobs')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'addsimple')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        job_name = selenium.find_element(By.ID,'name')
+        job_description = selenium.find_element(By.ID,'description')
+        job_message = selenium.find_element(By.ID,'message')
+        job_category = selenium.find_element(By.ID,'category')
+        job_device = selenium.find_element(By.ID,'currdevice')
+        job_cron_check =selenium.find_element(By.ID,'cronCheck')
+        job_cron_every_value =selenium.find_element(By.ID,'cronEveryValue')
+        job_cron_every =selenium.find_element(By.ID,'cronEvery')
+        submit_button =selenium.find_element(By.ID,'submit')
+
+    
+
+        #populate the form with data
+        job_name.send_keys('Send Email Test')
+        job_description.send_keys('Send an email')
+        job_message.send_keys('This is a test message')
+        #Select dropdown values
+        job_cron_check.click()
+
+        drop_category= Select(job_category)
+        drop_category.select_by_index(2)
+
+        drop_device= Select(job_device)
+        drop_device.select_by_index(0)
+
+        drop_cron_every= Select(job_cron_every)
+        drop_cron_every.select_by_index(1)
+
+        drop_cron_every_value= Select(job_cron_every_value)
+        drop_cron_every_value.select_by_index(5)
+
+        
+
+    
+        
+    
+        time.sleep(1)
+        #submit form
+        #submit_button.click()
+        time.sleep(2)
+
+    def testmultijobform(self):
+        selenium = webdriver.Chrome()
+         #Choose your url to visit
+        selenium.get('http://localhost:9000/terra-pi/')
+        time.sleep(1)
+        #navigate to jobs page
+        selenium.get('http://localhost:9000/terra-pi/jobs')
+        time.sleep(1)
+        add = selenium.find_element(By.ID,'addmulti')
+        add.click()
+        time.sleep(1)
+
+        #find the elements you need to submit form
+        job_name = selenium.find_element(By.ID,'name')
+        job_description = selenium.find_element(By.ID,'description')
+        job_message = selenium.find_element(By.ID,'message')
+        job_category = selenium.find_element(By.ID,'category')
+        job_device = selenium.find_element(By.ID,'currdevice')
+        job_cron_check =selenium.find_element(By.ID,'cronCheck')
+        job_cron_every_value =selenium.find_element(By.ID,'cronEveryValue')
+        job_cron_every =selenium.find_element(By.ID,'cronEvery')
+        submit_button =selenium.find_element(By.ID,'submit')
+
+    
+
+        #populate the form with data
+        job_name.send_keys('TempNotificationOff')
+        job_description.send_keys('Read temp, send notification, turn device off')
+        job_message.send_keys('This is a test message')
+        #Select dropdown values
+        job_cron_check.click()
+
+        drop_category= Select(job_category)
+        drop_category.select_by_index(2)
+
+        drop_device= Select(job_device)
+        drop_device.select_by_index(0)
+
+        drop_cron_every= Select(job_cron_every)
+        drop_cron_every.select_by_index(1)
+
+        drop_cron_every_value= Select(job_cron_every_value)
+        drop_cron_every_value.select_by_index(5)
+
+        
+
+    
+        
+    
+        time.sleep(1)
+        #submit form
+        #submit_button.click()
+        time.sleep(2)
+       
--- frontEnd/views.py
+++ frontEnd/views.py
@@ -3,94 +3,96 @@ from django.http import HttpResponse
 import requests
 from . import appHelper
 
-
+serverURL = "http://localhost:8080/"
 # Create your views here.
 def home_page(request):
     #do stuff
     #send emails
-    return render(request, 'home.html',{'name':'albert'})
+    response = requests.get(serverURL+"data-entry/view-all")
+    dataEntry = response.json()
+    return render(request, 'home.html',{'dataEntry':dataEntry,'serverURL':serverURL})
 
 ### Devices###
 def devices_page(request):
     #do stuff
     #send emails
-    response = requests.get("http://127.0.0.1:8080/device/view-all?category=device")
+    response = requests.get(serverURL+"device/view-all?category=device")
     devices = response.json()
-    response = requests.get("http://127.0.0.1:8080/device/view-all?category=notification")
+    response = requests.get(serverURL+"device/view-all?category=notification")
     notifications = response.json()
-    return render(request, 'devices.html',{'devices':devices, 'notifications':notifications})
+    return render(request, 'devices.html',{'devices':devices, 'notifications':notifications,'serverURL':serverURL})
     
 
 def new_device_page(request):
     #do stuff
     #send emails
     GPIOs = appHelper.createGPIOList()
-    return render(request, 'device.html',{'GPIOs':GPIOs})
+    return render(request, 'device.html',{'GPIOs':GPIOs,'serverURL':serverURL})
 
 def edit_device_page(request,id):
     #do stuff
     #send emails
-    response = requests.get("http://127.0.0.1:8080/device/view/"+id)
+    response = requests.get(serverURL+"device/view/"+id)
     device = response.json()
     GPIOs = appHelper.createGPIOList()
-    return render(request, 'device.html',{'device':device,'GPIOs':GPIOs})
+    return render(request, 'device.html',{'device':device,'GPIOs':GPIOs,'serverURL':serverURL})
 
  ###Sensors###  
 def sensors_page(request):
     #do stuff
     #send emails
-    response = requests.get("http://127.0.0.1:8080/device/view-all?category=sensor")
+    response = requests.get(serverURL+"device/view-all?category=sensor")
     devices = response.json()
     GPIOs = appHelper.createGPIOList()
-    return render(request, 'sensors.html',{'devices':devices})
+    return render(request, 'sensors.html',{'devices':devices,'serverURL':serverURL})
     
 
 def new_sensor_page(request):
     #do stuff
     #send emails
     GPIOs = appHelper.createGPIOList()
-    return render(request, 'sensor.html',{'GPIOs':GPIOs})
+    return render(request, 'sensor.html',{'GPIOs':GPIOs,'serverURL':serverURL})
 
 def edit_sensor_page(request,id):
     #do stuff
     #send emails
-    response = requests.get("http://127.0.0.1:8080/device/view/"+id)
+    response = requests.get(serverURL+"device/view/"+id)
     device = response.json()
     GPIOs = appHelper.createGPIOList()
-    return render(request, 'sensor.html',{'device':device,'GPIOs':GPIOs})
+    return render(request, 'sensor.html',{'device':device,'GPIOs':GPIOs,'serverURL':serverURL})
 
 ### Jobs ###
 def jobs_page(request):
     #dostuff
-    response = requests.get("http://127.0.0.1:8080/job/view-all")
+    response = requests.get(serverURL+"job/view-all")
     jobs = response.json()
-    return render(request, 'jobs.html',{'jobs':jobs})
+    return render(request, 'jobs.html',{'jobs':jobs,'serverURL':serverURL})
 
 def new_simple_job_page(request):
     #dostuff
     
-    return render(request, 'job_simple.html',{'job':''})
+    return render(request, 'job_simple.html',{'serverURL':serverURL})
 
 def new_multi_job_page(request):
     #dostuff
-    return render(request, 'job_multi.html',{'job':''})
+    return render(request, 'job_multi.html',{'serverURL':serverURL})
 
 def edit_simple_job_page(request, id):
     #dostuff
-    response = requests.get("http://127.0.0.1:8080/job/view/"+id)
+    response = requests.get(serverURL+"job/view/"+id)
     job = response.json()
-    return render(request, 'job_simple.html',{'job':job})
+    return render(request, 'job_simple.html',{'job':job,'serverURL':serverURL})
 
 def edit_multi_job_page(request, id):
     #dostuff
-    response = requests.get("http://127.0.0.1:8080/job/view/"+id)
-    jobs = response.json()
-    return render(request, 'job_multi.html',{'':''})
+    response = requests.get(serverURL+"job/view/"+id)
+    job = response.json()
+    return render(request, 'job_multi.html',{'job':job,'serverURL':serverURL})
 
 
 ### About ###
 def about_page(request):
     #do stuff
     #send emails
-    return render(request, 'about.html',{'name':'albert'})
+    return render(request, 'about.html',{'serverURL':serverURL})
 
